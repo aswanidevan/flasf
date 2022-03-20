455d94ee3f9da7dfabdf144c687c6a8657fa635e
ashvini-kn <66940827+ashvini-kn@users.noreply.github.com>
2022-03-20 15:41:13 +0530

    first

---
+++ app/src/main/java/com/example/richflashlight/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.richflashlight;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+import java.util.Timer;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+      //  Handler().post
+
+
+    }
+}
